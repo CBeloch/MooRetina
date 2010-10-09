9640ecc39e4b79af7ac2d7a3c02b0a9e4627539d
Christopher Beloch <webmaster@cbeloch.de>
2010-10-09 22:22:33 +0200

    First Source Commit

---
+++ Source/MooRetina.js
@@ -0,0 +1,47 @@
+/*
+---
+
+script: MooRetina.js
+version: 1.0
+description: MooRetina enhances your website for devices with retina display by loading images with a higher resolution
+license: MIT-style
+authors:
+- Troy Mcilvena
+- Christopher Beloch
+
+requires: 
+  core:1.2.5: [Element, Request]
+
+provides: [MooRetina]
+
+...
+*/
+
+var MooRetina = new Class({
+
+	Implements: [Options],
+	
+	options: {
+		suffix: '-2x' // retina version of image.jpg is image-2x.jpg
+	},
+	
+	initialize: function(elements,options){
+		this.setOptions(options);
+		this.elements = elements || $$('img');
+		
+		if(!window.devicePixelRatio || window.devicePixelRatio < 2) // Just if it's a Retina Device (iPhone 4 or 4th Gen iPod Touch)
+		{ return; }
+		
+		this.elements.each(function(imageElement){
+			var src = imageElement.get('src');
+			var newSrc = src.replace(/(.+)(\.\w{3,4})$/, "$1"+ this.options.suffix +"$2");
+			
+			var checkImage = new Request({
+				url: newSrc,
+				onSuccess: function(){
+					imageElement.set('src', newSrc);
+				}
+			}).send();	
+		}, this);
+	}
+});
